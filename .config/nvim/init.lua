f5deddab01d1641a9825da85a4e3e6c131dedb55
DnOnith <leonard.k.brandt@web.de>
2026-02-05 18:30:00 +0100

    nvim configuration
    Improved vimtex, changed local leader to ß. Added mason, lspconfig and
    mason-lspconfig

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,7 +1,9 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
+--vim.g.maplocalleader = "ß"
+vim.cmd([[let maplocalleader = "ß"]])
 require("config.lazy")
 
 vim.cmd([[
 set clipboard+=unnamedplus
-
+let maplocalleader = "ß"
 ]])
