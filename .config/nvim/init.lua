71c38f07f7a336a6d0b1501b9af6756fe9e1c077
DnOnith <leonard.k.brandt@web.de>
2026-02-19 11:01:22 +0100

    added Rust plugins/lsp to nvim
    Mostly through the LazyVim Extras

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 --vim.g.maplocalleader = "ß"
 vim.cmd([[let maplocalleader = "ß"]])
+--vim.keymap.set("n", "<leader>ö", vim.diagnostic.goto_next, opts)
+--vim.keymap.set("n", "<leader>a", vim.lsp.buf.code_action, opts)
 require("config.lazy")
 
 vim.cmd([[
