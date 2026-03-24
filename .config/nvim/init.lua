6baba11bf3d1bf5741735981763c6863ed25f77f
DnOnith <leonard.k.brandt@web.de>
2026-03-24 14:12:28 +0100

    finally understood how lazyvim actually wants some of the config
    moved the options into options.lua, also removes warning everytime you
    start nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,11 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
---vim.g.maplocalleader = "ß"
-vim.cmd([[let maplocalleader = "ß"]])
---vim.keymap.set("n", "<leader>ö", vim.diagnostic.goto_next, opts)
---vim.keymap.set("n", "<leader>a", vim.lsp.buf.code_action, opts)
 require("config.lazy")
-
-vim.cmd([[
-set clipboard+=unnamedplus
-let maplocalleader = "ß"
-]])
