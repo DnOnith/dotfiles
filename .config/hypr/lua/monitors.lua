edb9d3b5a9cbfa282394400a17ac2e12fb27d5f2
Leonard Brandt <leonard.k.brandt@web.de>
2026-05-13 15:19:16 +0200

    adjusted main monitor scale, fixed small syntax error

--- .config/hypr/lua/monitors.lua
+++ .config/hypr/lua/monitors.lua
@@ -6,12 +6,12 @@ hl.monitor({
 	output = "eDP-1",
 	mode = "2560x1440@120",
 	position = "0x0",
-	scale = "1.60",
+	scale = "1.33",
 })
 
 hl.monitor({
 	output = "",
 	mode = "preferred",
 	position = "auto",
-	scale = 1,
+	scale = "1",
 })
