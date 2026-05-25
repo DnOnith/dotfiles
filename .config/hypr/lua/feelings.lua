ac56c540e5773b00ea75efb35d920b9bceabc723
Leonard Brandt <leonard.k.brandt@web.de>
2026-05-25 17:43:55 +0200

    couple of small visual changes to make the system feel more focused and utilitarian

--- .config/hypr/lua/feelings.lua
+++ .config/hypr/lua/feelings.lua
@@ -2,8 +2,8 @@
 
 hl.config({
 	general = {
-		gaps_in = 5,
-		gaps_out = 20,
+		gaps_in = 2,
+		gaps_out = 10,
 
 		border_size = 2,
 
@@ -23,7 +23,7 @@ hl.config({
 	},
 
 	decoration = {
-		rounding = 10,
+		rounding = 1,
 		rounding_power = 2,
 
 		-- Change transparency of focused and unfocused windows
