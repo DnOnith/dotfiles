4cc60d34c04d0530ddb1ef0778b00a9930c0fdd7
Leonard Brandt <leonard.k.brandt@web.de>
2026-06-30 17:28:52 +0200

    didnt commit for a while so some minimal changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,20 +7,18 @@ function fish_greeting
 end
 
 alias py='python3'
-alias encode=". /home/leo/.config/scripts/encode.sh"
-alias nnn="nnn -eHC"
 alias wiki="zen-browser 'wiki.archlinux.org/title/Main_page'"
-alias environment="source venv/bin/activate"
+alias environment="source venv/bin/activate.fish"
 alias c="clear"
 alias sacman="sudo pacman"
 alias hconf="nvim ~/.config/hypr/"
-alias opro="cd ~/gypt/gypt25/ && environment"
 alias hdmi='hyprctl keyword monitor HDMI-A-2, preferred, auto, 1'
 alias hdmi='hyprctl keyword monitor HDMI-A-2, disable'
 alias fd='fd -I' #fd doesn't really work otherwise because most of my home directory is ignored in .gitignore for the dot repo
 export NVM_DIR="$HOME/.nvm"
 alias lg='lazygit'
 alias arcli='arduino-cli'
+alias trs ='trash-put'
 
 function mkcd
     command mkdir $argv
